2843ee3bd8b894b47b85e97b6857fa11593ec625
Murtada al Mousawy <murtada.al.mousawy@gmail.com>
2019-04-18 14:20:38 +0200

    Add TypeScript + Rollup support for gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,41 @@
 const { src, dest, series, parallel } = require('gulp');
+const path = require('path');
+const rollup = require('rollup');
+const resolve = require('rollup-plugin-node-resolve');
+const typescript = require('rollup-plugin-typescript2');
 
-function build(done) {
-    done();
+const config = require('./config.json');
+
+function htmlTask() {
+    return src(config.html.src, {
+        cwd: config.src_base
+    })
+    .pipe(dest(config.dest_base));
 }
 
-exports.default = build;
+function typescriptTask() {
+    return rollup.rollup({
+        input: path.join(config.src_base, config.typescript.src),
+        plugins: [
+            resolve({
+                main: true,
+                browser: true
+            }),
+            typescript()
+        ]
+    }).then(bundle => {
+        return bundle.write({
+            file: path.join(config.dest_base, config.typescript.dest),
+            format: 'umd',
+            name: 'library',
+            sourcemap: true
+        });
+    });
+}
+
+exports.typescriptTask = typescriptTask;
+
+exports.default = parallel(
+    htmlTask,
+    typescriptTask
+);
